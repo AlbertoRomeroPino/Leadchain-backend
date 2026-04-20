cca1bba205a45952a33fe2a2c362b6338813b7cf
Your Name <you@example.com>
2026-04-20 20:29:22 +0200

    seeder test

---
+++ database/seeders/TestingSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class TestingSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $this->call(EstadoVisitaSeeder::class);
+
+        User::updateOrCreate(
+            ['email' => 'root@leadchain.local'],
+            [
+                'nombre' => 'Admin',
+                'apellidos' => 'Testing 1',
+                'password' => Hash::make('12345678'),
+                'rol' => 'admin',
+                'id_zona' => null,
+                'id_responsable' => null,
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'root2@leadchain.local'],
+            [
+                'nombre' => 'Admin',
+                'apellidos' => 'Testing 2',
+                'password' => Hash::make('12345678'),
+                'rol' => 'admin',
+                'id_zona' => null,
+                'id_responsable' => null,
+            ]
+        );
+    }
+}
